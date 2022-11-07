837dc1ebe46788351b8e4ee3335e45ed4c207a25
satya bhusan prusty <satyabhusan9853@gmail.com>
2022-11-07 20:13:24 +0530

    Added more Customized and detailed Errors

--- middlewares/Error-handler.js
+++ middlewares/Error-handler.js
@@ -1,11 +1,30 @@
 const { StatusCodes } = require("http-status-codes");
 
 const ErrorHandlerMiddleware = (error, req, res, next) => {
-  console.log(error);
+  console.log(error.errors);
   let custom = {
     message: error.message || "Something went wrong try again later",
     statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
   };
+
+  if (error.code && error.code === 11000) {
+    custom.message = `Duplicate value is entered for ${Object.keys(
+      error.keyValue
+    )} please enter unique value`;
+    custom.statusCode = 400;
+  }
+  if (error.name === "Validation Error") {
+    custom.message = Object.values(error.errors)
+      .map((item) => item.message)
+      .join(",");
+    custom.statusCode = 400;
+  }
+
+  if (error.name === "CastError") {
+    custom.message = `No item found with id: ${error.value}`;
+    custom.statusCode = 404;
+  }
+
   return res.status(custom.statusCode).json({ message: custom.message });
 };
 
